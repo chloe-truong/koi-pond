5ee7fd46885f553f9bc667332fea0b4059a4b2b0
chloe-truong <70724629+chloe-truong@users.noreply.github.com>
2020-11-29 10:17:27 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+echo "# koi-pond" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/chloe-truong/koi-pond.git
+git push -u origin main
+                
